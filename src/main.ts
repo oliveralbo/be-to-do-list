76f4ac0cdfd5d3b0259fd1a72c4641ed457aa005
oliveralbo <oliveralbo@hotmail.com>
2025-05-31 16:37:08 -0300

    fix: patch crypo conditional2

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 if (!globalThis.crypto) {
+  // En Node 18, lo agregamos como propiedad
   Object.defineProperty(globalThis, 'crypto', {
     // eslint-disable-next-line @typescript-eslint/no-require-imports
     value: require('crypto'),
   });
 }
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
