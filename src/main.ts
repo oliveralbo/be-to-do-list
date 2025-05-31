cddd4155754ab5a2e56127e4171c01aa8f2f8ed8
oliveralbo <oliveralbo@hotmail.com>
2025-05-31 16:49:33 -0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 if (!globalThis.crypto) {
-  // En Node 18, lo agregamos como propiedad
   Object.defineProperty(globalThis, 'crypto', {
     // eslint-disable-next-line @typescript-eslint/no-require-imports
     value: require('crypto'),
@@ -11,6 +10,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'https://fe-to-do-list-2wyu74pjb-oliveralbos-projects.vercel.app/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
