79a1beee30af425eed48cae8045fc304bc4cf344
oliveralbo <oliveralbo@hotmail.com>
2025-05-31 16:27:23 -0300

    fix: patch crypto for typeorm compatibility

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-require-imports
+(global as any).crypto = require('crypto');
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
