5d18e6959042fb8a4ad2beff72ec4250af768bcf
oliveralbo <oliveralbo@hotmail.com>
2025-05-31 14:30:06 -0300

    feat: configure env.production

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import * as dotenv from 'dotenv';
-dotenv.config();
-
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
@@ -12,25 +9,49 @@ import { join } from 'path';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath:
+        process.env.NODE_ENV === 'production' ? '.env.production' : '.env',
+      isGlobal: true,
+    }),
+
     // GraphQL
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      playground: true, //Graphql PlayGround DOCs.
+      playground: true,
     }),
 
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '5432'),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // muestra las querys ?
+    TypeOrmModule.forRootAsync({
+      useFactory: () => {
+        const isProd = process.env.NODE_ENV === 'production';
+
+        if (isProd) {
+          return {
+            type: 'postgres',
+            url: process.env.DATABASE_URL,
+            synchronize: true,
+            autoLoadEntities: true,
+          };
+        }
+
+        return {
+          type: 'postgres',
+          host: process.env.DB_HOST,
+          port: parseInt(process.env.DB_PORT || '5432'),
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_NAME,
+          synchronize: true,
+          autoLoadEntities: true,
+        };
+      },
     }),
+
     TasksModule,
   ],
   controllers: [AppController],
